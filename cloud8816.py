925b1834aa9ce3943c6acf3d337580f4c50aba59
Flavio Pace <flavio.pace@gmail.com>
2024-07-23 21:52:04 +0200

    start to add some code

--- cloud8816.py
+++ cloud8816.py
@@ -1,1 +1,26 @@
+import os
+import sys
+import json
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+JSON_FILE = 'config.json'
+
+
+def demo(url):
+    driver = webdriver.Chrome()
+    driver.get(url)
+  
+    username = driver.find_element((By.XPATH, "//*/select[@ng-model='login.username']"))
+  
+    print(username)
+
+    driver.close()
+
+
+if __name__ == '__main__':
+     
+    with open(os.path.join(sys.path[0], JSON_FILE), 'r') as in_file:
+        conf = json.load(in_file)
+    demo(conf['cloud8816']['hostname'])
